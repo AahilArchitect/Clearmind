bca7ffd9d78d0c5e4617c7993889a54720df9078
AahilArchitect <110262226+AahilArchitect@users.noreply.github.com>
2026-05-21 12:31:09 -0400

    Add documentation comments to Main.java
    Added author and modification details in comments.

--- ClearMind/src/main/java/com/clearmind/Main.java
+++ ClearMind/src/main/java/com/clearmind/Main.java
@@ -1,3 +1,12 @@
+/**
+***************************************************************************************************
+@Author: Aahil Bashar
+@Last Modified: May 21th, 2026
+@Description: This is a main menu system with a Start button. Currently, the start button
+starts the notes editor the user is able to type on it
+***************************************************************************************************
+*/
+
 package com.clearmind;
 
 import javafx.application.Application;
@@ -8,7 +17,19 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    
+ /*
+ * Method Name: start
+ * Author: Aahil Bashar
+ * Creation Date: May 15, 2026
+ * Modified Date: May 20, 2026
+ * Description: Initializes the primary JavaFX stage, loads the ClearMind logo, and displays the main menu UI from an FXML file.
+ * Parameters: Stage stage (the primary window provided by the JavaFX runtime)
+ * Return Value: None
+ * Data Type: void
+ * Dependencies: javafx.stage.Stage, javafx.scene.Scene, javafx.scene.Parent, javafx.fxml.FXMLLoader, javafx.scene.image.Image
+ * Throws/Exceptions: Exception (if FXML or resource loading fails)
+ */
     @Override
     public void start(Stage stage) throws Exception {
 
